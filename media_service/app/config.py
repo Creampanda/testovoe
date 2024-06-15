fe5f54d621edd6927bb6ca43b6d097a770b0aef9
creampanda <andrewfromtokyo@gmail.com>
2024-06-15 14:06:55 +0300

    Remove unused

--- media_service/app/config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-POSTGRES_USER = os.environ.get("POSTGRES_USER", "admin")
-POSTGRES_PASSWORD = os.environ.get("POSTGRES_PASSWORD", "admin")
-POSTGRES_DB = os.environ.get("POSTGRES_DB", "postgres")
-MINIO_ROOT_USER = os.environ.get("MINIO_ROOT_USER", "minioadmin")
-MINIO_ROOT_PASSWORD = os.environ.get("MINIO_ROOT_PASSWORD", "minioadmin")
